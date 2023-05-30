cbc57f38b3b1eca14f0db2ecc4e133a6a88f90e7
BiaCalifornia13 <bia.sdca16@gmail.com>
2023-05-30 17:09:37 -0300

    modified:   Trabalho_Final/Jewel_Collector/JewelCollector.cs 	modified:   Trabalho_Final/Jewel_Collector/Jewel_Collector.csproj 	deleted:    Trabalho_Final/Jewel_Collector/Program.cs

--- Trabalho_Final/Jewel_Collector/JewelCollector.cs
+++ Trabalho_Final/Jewel_Collector/JewelCollector.cs
@@ -1,5 +1,32 @@
 namespace Jewel_Collector;
-
+using System;
 public class JewelCollector
 {
+      public static void Main() {
+  
+      bool running = true;
+  
+      do {
+  
+          Console.WriteLine("Enter the command: ");
+          ConsoleKeyInfo cki;
+          //string command = Console.ReadKey();
+          cki = Console.ReadKey();
+  
+          if (cki.Equals("quit")) {
+              running = false;
+          } else if (cki.Equals("w")) {
+              
+          } else if (cki.Equals("a")) {
+              
+          } else if (cki.Equals("s")) {
+            
+          } else if (cki.Equals("d")) {
+          
+          } else if (cki.Equals("g")) {
+              
+          }
+          
+      } while (running);
+  }
 }
