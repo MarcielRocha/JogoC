56ed4a66373a7f7d5852d9957ba2687a4f5f49bb
MarcielRocha <marcielrochapj@gmail.com>
2023-05-31 17:23:48 -0300

    modified:   Trabalho_Final/Jewel_Collector/Jewel.cs

--- Trabalho_Final/Jewel_Collector/Jewel.cs
+++ Trabalho_Final/Jewel_Collector/Jewel.cs
@@ -2,6 +2,15 @@ namespace Jewel_Collector;
 
 public class Jewel
 {
+    private int posicaox;
+    private int posicaoy;
 
-    //Teste
+    private string tipo;
+
+    public Jewel(int x, int y)
+    {
+        posicaox = x;
+        posicaoy = y;
+    }
+    
 }
