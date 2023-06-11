1628e6235fc08732140ae5daa229bce14969726c
MarcielRocha <marcielrochapj@gmail.com>
2023-06-11 07:45:39 -0300

    V4 	modified:   Trabalho_Final/Jewel_Collector/Jewel.cs 	modified:   Trabalho_Final/Jewel_Collector/Map.cs 	modified:   Trabalho_Final/Jewel_Collector/Obstacle.cs 	modified:   Trabalho_Final/Jewel_Collector/Robot.cs

--- Trabalho_Final/Jewel_Collector/JewelCollector.cs
+++ Trabalho_Final/Jewel_Collector/JewelCollector.cs
@@ -23,72 +23,63 @@ public class JewelCollector
     /// Carrega o mapa e inicia o jogo. 
     /// A cada nova letra, recarrega o mapa já com a nova acao realizada.
     /// </summary>
-      public static void Main() {
-  
-      bool running = true;
+      public static void Main() 
+      {
+            bool running = true;
+            int w = 10;
+            int h = 10;
+            int level = 1;
 
-      int w = 10;
-      int h = 10;
-      int level = 1;
-  
-      do {
-  
             Map map = new Map (w, h, level);
             Robot robot = new Robot(map);
 
-            Console.WriteLine($"Level: {level}");
-
-            try{
-                bool Result = Run(robot);
-                if(Result)
-                {
-                    w++;
-                    h++;
-                    level++;
-                }
-                else
-                {
-                    break;
-                }
-            }
-            catch(RanOutOfEnergyException e)
+            OnGoUp += robot.GoUp;
+            OnGoDown += robot.GoDown;
+            OnGoRight += robot.GoRight;
+            OnGoLeft += robot.GoLeft;
+            OnGet += robot.GetJ;
+        
+            do 
             {
-                Console.WriteLine("Robot ran out of energy!");
-            }
-          
-      } while (running);
-  }
-    /// <summary>
-    /// Ler o teclado e executar a acao indicada.
-    /// </summary>
-    private static bool Run(Robot robot)  
-    {
-        OnGoUp += robot.GoUp;
-        OnGoDown += robot.GoDown;
-        OnGoRight += robot.GoRight;
-        OnGoLeft += robot.GoLeft;
-        OnGet += robot.GetJ;
+        
+                if(!robot.HasEnergy()) throw new RanOutOfEnergyException();
+                Console.Clear();
+                robot.Print();
+                Console.WriteLine("\n Digite o comando(W,S,D,A,G,Q): ");
+                ConsoleKeyInfo command = Console.ReadKey(true);
 
-        do {
-            if(!robot.HasEnergy()) throw new RanOutOfEnergyException();
-            robot.Print();
-            Console.WriteLine("\n Enter the command: ");
-            ConsoleKeyInfo command = Console.ReadKey(true);
+                bool Result = true;
 
-            switch (command.Key.ToString())
-            {
-                case "W": Console.WriteLine($"\n Comando:{command.Key.ToString()}"); OnGoUp() ; break;
-                case "S" : Console.WriteLine($"\n Comando:{command.Key.ToString()}"); OnGoDown() ; break;
-                case "D" : Console.WriteLine($"\n Comando:{command.Key.ToString()}"); OnGoRight() ; break;
-                case "A" : Console.WriteLine($"\n Comando:{command.Key.ToString()}"); OnGoLeft() ; break;
-                case "G" : Console.WriteLine($"\n Comando:{command.Key.ToString()}"); OnGet() ; break;
-                case "Q" : return false;
-                default: Console.WriteLine($"\n Comando inválido:{command.Key.ToString()}"); break;
-            }
-        } while (!robot.map.IsDone());
-        return true;
-    }
+                switch (command.Key.ToString())
+                {
+                    case "W": Console.WriteLine($"\n Comando:{command.Key.ToString()}"); OnGoUp() ; break;
+                    case "S" : Console.WriteLine($"\n Comando:{command.Key.ToString()}"); OnGoDown() ; break;
+                    case "D" : Console.WriteLine($"\n Comando:{command.Key.ToString()}"); OnGoRight() ; break;
+                    case "A" : Console.WriteLine($"\n Comando:{command.Key.ToString()}"); OnGoLeft() ; break;
+                    case "G" : Console.WriteLine($"\n Comando:{command.Key.ToString()}"); OnGet() ; break;
+                    case "Q" : Result = false; break;
+                    default: Console.WriteLine($"\n Comando inválido:{command.Key.ToString()}"); break;
+                }
 
+                try
+                {
+                    if(Result)
+                    {
+                        w++;
+                        h++;
+                        level++;
+                    }
+                    else break;
+                }
+                catch(RanOutOfEnergyException e)
+                {
+                    Console.WriteLine("Nível crítico de energia!");
+                }
+
+                Console.WriteLine($"Estágio: {level}");
+                
+            } while (running);
+        }
 }
 
 static class Constants
